5e363828e4ab6dfb18c0a69b64553331a180ce8e
step_ne <nemail.necom@gmail.com>
2020-06-28 16:24:28 +0300

    Created app

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+    return (
+        <div>
+            Hello World!
+        </div>
+    )
+}
+//
+// function App() {
+//   return (
+//     <div className="App">
+//       <h2>Hello!</h2>
+//     </div>
+//   );
+// }
+//
+// export default App;
+export default App;
